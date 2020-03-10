ae0a19e109a56a32b99a38d0feef087f584741d1
faizhameed <faizftb@gmail.com>
2020-03-10 18:14:42 +0530

    changed env variable

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":750,"linkImagesToOriginal":false},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-templates-blog-jsx": hot(preferDefault(require("/home/faiz/Documents/Gatsby-Projects/gatsby-contentful/src/templates/blog.jsx"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/faiz/Documents/Gatsby-Projects/gatsby-contentful/.cache/dev-404-page.js"))),
+  "component---src-pages-404-jsx": hot(preferDefault(require("/home/faiz/Documents/Gatsby-Projects/gatsby-contentful/src/pages/404.jsx"))),
+  "component---src-pages-about-jsx": hot(preferDefault(require("/home/faiz/Documents/Gatsby-Projects/gatsby-contentful/src/pages/about.jsx"))),
+  "component---src-pages-blog-jsx": hot(preferDefault(require("/home/faiz/Documents/Gatsby-Projects/gatsby-contentful/src/pages/blog.jsx"))),
+  "component---src-pages-contact-jsx": hot(preferDefault(require("/home/faiz/Documents/Gatsby-Projects/gatsby-contentful/src/pages/contact.jsx"))),
+  "component---src-pages-index-jsx": hot(preferDefault(require("/home/faiz/Documents/Gatsby-Projects/gatsby-contentful/src/pages/index.jsx")))
+}
+
