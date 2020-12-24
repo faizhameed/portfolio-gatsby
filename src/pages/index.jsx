25489616090c29ed5c8bf4931162759c47fb26dc
Faiz Hameed <faizftb@gmail.com>
2020-12-24 22:43:44 +0530

    added illustrations and styles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import CoderImg from "../assets/svg/coder.svg";
 
 import Layout from "../components/layout";
 import Head from "../components/head";
+
+import styles from "./index.module.scss";
+
 const IndexPage = () => {
   return (
     <Layout>
       <Head title="Home" />
       <React.Fragment>
-        <div>
-          <h1>Faiz Hameed</h1>
-          <p>Full Stack Web Developer</p>
+        <div className={styles.container}>
+          <div>
+            <h1>Faiz Hameed</h1>
+            <p>Full Stack Web Developer</p>
+          </div>
+          <img className={styles.coderImg} src={CoderImg} alt="coder" />
         </div>
       </React.Fragment>
     </Layout>
