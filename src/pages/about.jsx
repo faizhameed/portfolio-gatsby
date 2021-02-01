4258ba3e6ec4288f63a7d6ac9f645405f5dec28b
Faiz Hameed <faizftb@gmail.com>
2021-02-01 21:36:45 +0530

    about us change

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,7 +8,11 @@ const AboutPage = () => {
       <Head title="About-us" />
       <div>
         <h1>About us</h1>
-        <p>working in corefactors bangalore</p>
+        <p>
+          Mechanical Engineer by degree. I worked for a company in Saudi Arabia.
+          The work culture didnt fit me, I was missing something in life. It was
+          then I started going deeper into doing things that gives me pleasure
+        </p>
       </div>
     </Layout>
   );
