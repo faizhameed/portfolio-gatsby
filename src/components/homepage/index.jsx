bc8ca925f5f7ffd0624d5f55f7548449c3639088
Faiz Hameed <faizftb@gmail.com>
2021-02-05 22:19:13 +0530

    fix project sectioN

--- src/components/homepage/index.jsx
+++ src/components/homepage/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./index.module.scss";
 import CoderImg from "../../assets/svg/coder.svg";
+import { MyProjects } from "./enum";
 
 const HomePage = () => {
   return (
@@ -25,8 +26,15 @@ const HomePage = () => {
         <img className={styles.coderImg} src={CoderImg} alt="coder" />
       </div>
       <div>
-        <h2>My Projects</h2>
-        <div>{}</div>
+        <h2 className={styles.prj_heading}>My Works</h2>
+        <div>
+          {MyProjects.map((item) => (
+            <div>
+              <div className={styles.prj_title}>{item.name}</div>
+              <div>{item.description}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </React.Fragment>
   );
