f3ccddeab4b827d8ab24c3b214aa3a7e993fd2df
Faiz Hameed <faizftb@gmail.com>
2021-02-15 22:00:52 +0530

    added more content

--- src/components/homepage/enum.js
+++ src/components/homepage/enum.js
@@ -2,5 +2,21 @@ export const MyProjects = [
   {
     name: "Ticktivity",
     description: "develop new product to increase the productivity of the user",
+    imgUrl: "",
+    link: "",
+  },
+  {
+    name: "Address Book",
+    description:
+      "Shows contact list of people populated from an API, features include scroll fetching, dark and light them, profile page etc",
+    imgUrl: "",
+    link: "",
+  },
+  {
+    name: "Image Lash",
+    description:
+      "Search and download images from unsplash, view images by tags etc",
+    imgUrl: "",
+    link: "imaglash.netlify.com",
   },
 ];
