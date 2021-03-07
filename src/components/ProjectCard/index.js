44ca6f178c0e0b596da27a129fa2dc36f8c83723
Faiz Hameed <faizftb@gmail.com>
2021-03-07 22:35:40 +0530

    creating linkt to projects

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,15 +1,18 @@
+import { Link } from "gatsby";
 import React from "react";
 import styles from "./index.module.scss";
 
-const ProjectCard = ({ projectTitle, imgUrl, description }) => {
+const ProjectCard = ({ projectTitle, imgUrl, description, link }) => {
   return (
-    <div className={styles.card}>
-      <div className={styles.prj_title}>{projectTitle}</div>
-      <div>
-        <img className={styles.image} src={imgUrl} alt="project" />
+    <Link to={link}>
+      <div className={styles.card}>
+        <div className={styles.prj_title}>{projectTitle}</div>
+        <div>
+          <img className={styles.image} src={imgUrl} alt="project" />
+        </div>
+        <div className={styles.description}>{description}</div>
       </div>
-      <div className={styles.description}>{description}</div>
-    </div>
+    </Link>
   );
 };
 
