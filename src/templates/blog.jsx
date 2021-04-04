2eac40862622119da5830714ec6b72af152f1d55
Faiz Hameed <faizftb@gmail.com>
2021-04-04 22:09:15 +0530

    migrating from gatsby 2.19.19 to 3.2.1

--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -21,7 +21,7 @@ export const query = graphql`
       title
       publishedDate(formatString: "MMMM Do, YYYY")
       body {
-        json
+        raw
       }
     }
   }
@@ -42,7 +42,7 @@ const Blog = (props) => {
       <h1>{props.data.contentfulBlogPost.title}</h1>
       <p>{props.data.contentfulBlogPost.publishedDate}</p>
       {documentToReactComponents(
-        props.data.contentfulBlogPost.body.json,
+        props.data.contentfulBlogPost.body.raw,
         options
       )}
     </Layout>
