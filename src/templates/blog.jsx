ccbc19c3dc86fe6cbf19c7c295cd7c44e397f58a
Faiz Hameed <faizftb@gmail.com>
2021-04-05 23:34:25 +0530

    ix issues witreact asset @contetnfu;

--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -36,13 +36,14 @@ const Blog = (props) => {
       },
     },
   };
+
   return (
     <Layout>
       <Head title={props.data.contentfulBlogPost.title} />
       <h1>{props.data.contentfulBlogPost.title}</h1>
       <p>{props.data.contentfulBlogPost.publishedDate}</p>
       {documentToReactComponents(
-        props.data.contentfulBlogPost.body.raw,
+        JSON.parse(props.data.contentfulBlogPost.body.raw),
         options
       )}
     </Layout>
