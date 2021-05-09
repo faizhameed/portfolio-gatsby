606105ed7dd3c7471e9b7db11d6331f0a1e2b757
Faiz Hameed <faizftb@gmail.com>
2021-05-09 23:17:13 +0530

    forom contact us input styling

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,31 +1,37 @@
 import React from "react";
+import { CONTACT_DATA } from "./enum";
 import styles from "./index.module.scss";
 
 const ContactForm = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.address}>
-        <h2>Where to reach me</h2>
+      <div className={styles.desc}>
+        <h2>{CONTACT_DATA.SUBTITLE}</h2>
+        <div>
+          <p>{CONTACT_DATA.DESCRIPTION}</p>
+        </div>
       </div>
       <form className={styles.form}>
-        <h2>Contact Us</h2>
-        <label>
-          Name
+        <h2>{CONTACT_DATA.TITLE}</h2>
+        <div className={styles.inputGroup}>
+          <label>Name</label>
           <input type="text" name="Name" />
-        </label>
-        <label>
-          Email
+        </div>
+        <div className={styles.inputGroup}>
+          <label>Email</label>
           <input type="emai" name="email" />
-        </label>
-        <label>
-          Phone
+        </div>
+        <div className={styles.inputGroup}>
+          <label>Phone</label>
           <input type="text" name="phone" />
-        </label>
-        <label>
-          Message
+        </div>
+        <div className={styles.inputGroup}>
+          <label>Message</label>
           <input type="text" name="message" />
-        </label>
-        <button type="submit">Submit</button>
+        </div>
+        <button className={styles.sbtn} type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
