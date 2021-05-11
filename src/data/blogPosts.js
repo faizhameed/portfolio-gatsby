50601e83892bccd610b0e573d7dbbdebda3b2ea5
Faiz Hameed <faizftb@gmail.com>
2021-05-11 23:56:23 +0530

    fix other posts to show

---
+++ src/data/blogPosts.js
@@ -0,0 +1,26 @@
+export const otherBlogPosts = [
+  {
+    node: {
+      title:
+        "How I transitioned from Mechanical Engineering into Web Developer Full Time",
+      publishedDate: "July 29 2019",
+      excerpt:
+        "Unarguably Mechanical Engineering is one of the best fields of engineering to pursue. But the taste and passion can be different for people",
+      Img: "https://miro.medium.com/max/7764/1*NlqpTTAM8DbGl4paBmjE_g.jpeg",
+      url:
+        "https://medium.com/zerotomastery/how-i-transitioned-from-mechanical-engineering-into-web-developer-full-time-3e7ddc236388",
+    },
+  },
+  {
+    node: {
+      title:
+        "New How I transitioned from Mechanical Engineering into Web Developer Full Time",
+      publishedDate: "July 29 2021",
+      excerpt:
+        "Unarguably Mechanical Engineering is one of the best fields of engineering to pursue. But the taste and passion can be different for people",
+      Img: "https://miro.medium.com/max/7764/1*NlqpTTAM8DbGl4paBmjE_g.jpeg",
+      url:
+        "https://medium.com/zerotomastery/how-i-transitioned-from-mechanical-engineering-into-web-developer-full-time-3e7ddc236388",
+    },
+  },
+];
