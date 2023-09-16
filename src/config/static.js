5bb92b87a3157898db52184b9c1e4ab702819b3d
Faiz <faizhameedv@gmail.com>
2023-09-16 13:47:04 +0530

    updating tagline

--- src/config/static.js
+++ src/config/static.js
@@ -1,6 +1,7 @@
 export const HOMEPAGE_DATA = {
   greet: "Hello !",
   name: "Faiz Hameed",
-  tagline: "Front End Engineer!",
-  tagline2: "I built products with React, Next and Gatsby",
+  subtitle: "Software Engineering at Google and Beyond",
+  tagline:
+    "Crafting innovative solutions, I've seamlessly blended the power of React, Next.js, Gatsby for the frontend with the flexibility of Node.js and Express for the backend to create exceptional products.",
 };
